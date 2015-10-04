dd6b3002b69035578c654a64c7ff7ebb02ca5809
rajatsri <rajatsrivastava@live.com>
2015-10-04 21:06:19 +1100

    Initial version

---
+++ src/main/java/com/logicbomb/newschool/HomePage.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.logicbomb.newschool;
+
+import com.logicbomb.newschool.widgets.UserDetailsWidget;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.AbstractComponent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ *
+ * @author Rajat
+ */
+@Theme("mytheme")
+@Widgetset("com.logicbomb.newschool.MyAppWidgetset")
+public class HomePage extends HorizontalLayout{
+
+    HomePage() {
+        
+        
+        final VerticalLayout frameA1 = new VerticalLayout();
+        frameA1.addStyleName("backColorGrey");
+        frameA1.setHeight("100%");
+        this.addComponent(frameA1);
+        
+        final VerticalLayout frameA2 = new VerticalLayout();
+        this.addComponent(frameA2);
+        
+        final VerticalLayout frameA3 = new VerticalLayout();
+        this.addComponent(frameA3);
+        
+        //Adding widgets to the frames
+        UserDetailsWidget u= new UserDetailsWidget();
+        frameA1.addComponent(u);
+        
+        
+        
+        
+        
+        
+
+        Button button = new Button("Click Me");
+        button.setWidth("100px");
+        button.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                //frameA2.addComponent(new Label("Thank you for clicking"));
+                UserDetailsWidget u1= new UserDetailsWidget();
+            frameA2.addComponent(u1);
+            }
+        });
+        frameA3.addComponent(button);
+
+    }
+}
---
+++ src/main/java/com/logicbomb/newschool/MyUI.java
@@ -0,0 +1,39 @@
+package com.logicbomb.newschool;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ *
+ */
+@Theme("mytheme")
+@Widgetset("com.logicbomb.newschool.MyAppWidgetset")
+public class MyUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) 
+    {
+        final VerticalLayout layout = new VerticalLayout();
+        //layout.setMargin(true);
+        setContent(layout);
+
+        HomePage listPage = new HomePage();
+        layout.addComponent(listPage);
+
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
