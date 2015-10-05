577ff89c4b0c6df93b1664bfe6d765a356fd4f97
aayushrajvanshi <aayushrajvanshii@gmail.com>
2015-10-06 01:39:18 +1100

    Login Widget added

--- src/main/java/com/loginbomb/newSchool/pages/LoginPage.java
+++ src/main/java/com/loginbomb/newSchool/pages/LoginPage.java
@@ -3,11 +3,7 @@ package com.loginbomb.newSchool.pages;
 import com.logicbomb.newschool.widgets.LoginWidget;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Widgetset;
-import com.vaadin.server.FileResource;
-import com.vaadin.server.VaadinService;
 import com.vaadin.ui.AbsoluteLayout;
-import com.vaadin.ui.Image;
-import java.io.File;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -23,21 +19,9 @@ import java.io.File;
 public class LoginPage extends AbsoluteLayout {
 
     public LoginPage() {
+        addStyleName("body");
         setWidth("100%");
         setHeight("800px");
-        // Find the application directory
-        String basepath = VaadinService.getCurrent()
-                .getBaseDirectory().getAbsolutePath();
-        // Image as a file resource
-        FileResource resource = new FileResource(new File(basepath
-                + "/WEB-INF/Images/loginPage.jpg"));
-
-        // Show the image in the application
-        Image iImage = new Image("", resource);
-        iImage.setWidth(100, Unit.PERCENTAGE);
-        
-        addComponent(iImage);
-
         LoginWidget iLoginWidget = new LoginWidget();
 
         ComponentPosition iComponentPosition = new ComponentPosition();
